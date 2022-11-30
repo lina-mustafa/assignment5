3be65a3eef00ac93c91f7b28d8a6a7f293746461
Mustafa <Lina.Mustafa@student.tdsb.on.ca>
2022-11-30 11:49:21 -0500

    Added componenets to site.

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Hero from './components/Hero.vue'
+import Footer from './components/Footer.vue'
+
+</script>
+
+<template>
+<HelloWorld></HelloWorld>
+<Header></Header>
+<Hero></Hero>
+<Footer></Footer>
+</template>
+
+<style scoped>
+</style>
