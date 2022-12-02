f9e43395428ef7f67db32a7c11203dc95b72e10e
Mustafa <Lina.Mustafa@student.tdsb.on.ca>
2022-12-02 11:40:16 -0500

    Added to components and created new folders.

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
-import Header from './components/Header.vue'
-import Hero from './components/Hero.vue'
-import Footer from './components/Footer.vue'
+import Header from './components/SiteHeader.vue'
+import Hero from './components/SiteHero.vue'
+import Footer from './components/SiteFooter.vue'
 
 </script>
 
---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,27 @@
+<script>
+</script>
+
+<template>
+    <h1>MoviesGo</h1> 
+    <br>
+    <button id="button">Login</button>
+    <img src="https://khusoko.com/wp-content/uploads/2022/02/Netflix.jpg">
+</template>
+
+<style scoped>
+button{
+    color: snow;
+    border-width: 2px;
+    size: 30px;
+}
+
+h1{
+    font-size: 80px;
+}
+
+img{
+    height:1024px;
+    width: 1280px;
+}
+
+</style>
