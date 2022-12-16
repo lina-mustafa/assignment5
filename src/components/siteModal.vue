25697d8fbc0e6017935c05df3f79472fd330b524
unknown <335036471@TDSB.ON.CA>
2022-12-16 11:04:35 -0500

    Finished project.

--- src/components/siteModal.vue
+++ src/components/siteModal.vue
@@ -5,15 +5,15 @@ import axios from "axios";
 const props = defineProps(["id"]);
 const response = ref(null);
 const emits = defineEmits(["toggleModal"]);
- 
+
 const getData = async (url, params) => {
-try {
-  return await axios.get(url, params);
-} catch (error) {
-  console.log(error);
-}
+  try {
+    return await axios.get(url, params);
+  } catch (error) {
+    console.log(error);
+  }
 };
- 
+
 const getMovies = async (movies) => {
   response.value = (await getData(`https://api.themoviedb.org/3/movie/${props.id}`, {
     params: {
@@ -21,6 +21,10 @@ const getMovies = async (movies) => {
       append_to_response: "videos",
     }
   })).data;
+
+
+  getMovies(props.id)
+  console.log(response.videos.results[0].key);
 }
 //const trailer = extraData.data.videos.results.filter((video) => video.type === "Trailer").at(0).key;
 
@@ -28,19 +32,20 @@ const getMovies = async (movies) => {
 
 <template>
   <Teleport to="body">
-    <div v-if= "getMovies(props.id)" class="modal-outer-container" @click.self="emits('toggleModal')">
+    <div class="modal-outer-container" @click.self="emits('toggleModal')">
       <div class="modal-inner-container">
         <button class="close-button" @click="emits('toggleModal')">X</button>
         <div class="description">
-        <p1>{{response.title}}</p1>
-        <br/>
-        <p2>Release date: {{response.release_date}}</p2>
-        <br/>
-        <p3>{{response.overview}}</p3>
-        <br/>
-        <p4><a href="https://www.youtube.com/embed/${data.videos.results[0].key}" target="_blank">Click here for the movie trailer!</a></p4>
-        <!--<iframe class="video" :src="`https://www.youtube.com/embed/${data.videos.results[0].key}?autoplay=1`" frameborder="0"></iframe>-->
-      </div>
+          <p1>{{ response.title }}</p1>
+          <br />
+          <p2>Release date: {{ response.release_date }}</p2>
+          <br />
+          <p3>{{ response.overview }}</p3>
+          <br />
+          <p4><a :href="`https://www.youtube.com/embed/${response.videos.results[0].key}`" target="_blank">Click here
+              for the movie trailer!</a></p4>
+          <!--<iframe class="video" :src="`https://www.youtube.com/embed/${data.videos.results[0].key}?autoplay=1`" frameborder="0"></iframe>-->
+        </div>
       </div>
     </div>
   </Teleport>
@@ -48,6 +53,7 @@ const getMovies = async (movies) => {
 
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Inconsolata&display=swap");
+
 .modal-outer-container {
   position: fixed;
   top: 0;
@@ -82,32 +88,37 @@ const getMovies = async (movies) => {
 
 p1 {
   font-size: 50px;
-  position: relative; top:30px;
+  position: relative;
+  top: 30px;
   font-family: 'Inconsolata', monospace;
 }
 
 p2 {
   font-size: 20px;
-  position: relative; top:70px;
+  position: relative;
+  top: 70px;
   font-family: 'Inconsolata', monospace;
 }
 
 p3 {
   font-size: 20px;
-  position: relative; top:90px;
+  position: relative;
+  top: 90px;
   font-family: 'Inconsolata', monospace;
 }
 
 p4 {
   font-size: 20px;
-  position: relative; top: 110px;
+  position: relative;
+  top: 110px;
   font-family: 'Inconsolata', monospace;
 }
-a{
+
+a {
   color: white;
 }
-a:hover{
+
+a:hover {
   color: rgb(229, 9, 20);
 }
-
 </style>
--- src/views/PurchaseView.vue
+++ src/views/PurchaseView.vue
@@ -2,8 +2,8 @@
 import { ref } from "vue";
 import axios from "axios";
 import SiteModal from '../components/SiteModal.vue';
- 
-const movies = ref ("");
+
+const movies = ref("");
 const response = ref(null);
 const showModal = ref(false);
 const selectedId = ref(0);
@@ -17,41 +17,39 @@ const closeModal = () => {
   showModal.value = false;
 };
 
+const getData = async (url, params) => {
+  try {
+    return await axios.get(url, params);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const getMovies = async () => {
-    console.log(movies.value)
-    response.value = (await getData(`https://api.themoviedb.org/3/trending/movie/week`, {
+  console.log(movies.value)
+  response.value = (await getData(`https://api.themoviedb.org/3/trending/movie/week`, {
     params: {
       api_key: "5ee6242bedc5de8c07acae66ee444042",
     }
-  })).data ['results'];
+  })).data['results'];
 }
- 
-const getData = async (url, params) => {
-try {
-  return await axios.get(url, params);
-} catch (error) {
-  console.log(error);
-}
-};
+
 await getMovies()
 
 </script>
 
 <template>
   <h1>Trending Movies:</h1>
-  <template v-if="response">
-  <div v-if="getMovies()" v-for="result in response" class="grid-container">
-  <!--<p>{{result.original_title}}</p>-->
-  <img @click="openModal(result.id)" v-bind:src="'http://image.tmdb.org/t/p/w500/' + result.poster_path">
+  <div v-for="result in response" class="grid-container">
+    <img @click="openModal(result.id)" v-bind:src="'http://image.tmdb.org/t/p/w500/' + result.poster_path">
   </div>
-  </template>
-  <SiteModal v-if="showModal" @toggleModal="closeModal()" :id="selectedId" ></SiteModal>
+  <SiteModal v-if="showModal" @toggleModal="closeModal()" :id="selectedId"></SiteModal>
 </template>
 
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap");
 
-.grid-container{
+.grid-container {
   display: flex;
   display: inline-grid;
   align-content: space-evenly;
@@ -60,15 +58,14 @@ await getMovies()
   margin-bottom: -260px;
 }
 
-img{
-  height:50%;
+img {
+  height: 50%;
   cursor: pointer;
 }
 
-h1{
+h1 {
   font-family: 'Bebas Neue', cursive;
   font-size: 100px;
   color: rgb(229, 9, 20);
 }
-
 </style>
